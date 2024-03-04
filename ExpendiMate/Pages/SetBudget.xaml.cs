cd239b0ead71746e12d5006ce5669198149fe69c
NiceFinal1230 <lohdiingyang@gmail.com>
2024-03-04 23:18:37 +0800

    Dynamic Update of UI Complete

--- ExpendiMate/Pages/SetBudget.xaml.cs
+++ ExpendiMate/Pages/SetBudget.xaml.cs
@@ -13,12 +13,6 @@ public partial class SetBudget : ContentPage
         InitializeComponent();
     }
 
-    //Update the view if property change
-    protected override void OnAppearing()
-    {
-        viewModel.OnPropertyChanged("User");
-    }
-
     private async void IncomeClicked(object sender, EventArgs e)
     {
 
@@ -61,5 +55,51 @@ public partial class SetBudget : ContentPage
     //        await DisplayAlert("Income", "Invalid input. Please enter a valid number.", "OK");
     //    }
     //}
+    private void SetBudget50(object sender, EventArgs e)
+    {
+        var model = viewModel.User;
+        model.BudgetPercent = 0.5;
+        SetBudgetViewModel.Current.SaveAndUpdateUser(model);
+    }
+    private void SetBudget30(object sender, EventArgs e)
+    {
+        var model = viewModel.User;
+        model.BudgetPercent = 0.3;
+        SetBudgetViewModel.Current.SaveAndUpdateUser(model);
+    }
+
+    private void SetBudget20(object sender, EventArgs e)
+    {
+        var model = viewModel.User;
+        model.BudgetPercent = 0.2;
+        SetBudgetViewModel.Current.SaveAndUpdateUser(model);
+    }
+
+    private async void SetBudgetManual(object sender, EventArgs e)
+    {
+        
+        string result = await DisplayPromptAsync("Budget Percentage", "Enter your budget percentage (1-100)");
+
+        if (double.TryParse(result, out double budgetPercent))
+        {
+            if (budgetPercent > 0 && budgetPercent <= 100)
+            {
+                var model = viewModel.User;
+                model.BudgetPercent = budgetPercent / 100;
+                SetBudgetViewModel.Current.SaveAndUpdateUser(model);
+            }
+            else
+            {
+                await DisplayAlert("Income", "Budget Percentage must be a number within 1-100", "OK");
+            }
+        }
+        else
+        {
+            await DisplayAlert("Income", "Invalid input. Please enter a valid number.", "OK");
+        }
+
+
+    }
+
 }
 
--- ExpendiMate/ViewModels/SetBudgetViewModel.cs
+++ ExpendiMate/ViewModels/SetBudgetViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using ExpendiMate.Models;
 using ExpendiMate.Services;
 using SQLite;
+using System.ComponentModel;
 
 namespace ExpendiMate.ViewModels
 {
@@ -14,6 +15,7 @@ namespace ExpendiMate.ViewModels
         {
             Current = this;
             connection = DatabaseServices.Connection;
+            UpdateView();
         }
 
         //Get user record
@@ -35,35 +37,174 @@ namespace ExpendiMate.ViewModels
             }
         }
 
-        public double BudgetBalance
+        private double _income;
+        public double Income
         {
-            get
+            get { return _income; }
+            set
             {
-                return User.Income - Expenses;
+                if (_income != value)
+                {
+                    _income = value;
+                    OnPropertyChanged(); // Raise PropertyChanged event
+                }
             }
         }
 
-        public double Expenses
+        private double _budgetBalance;
+        public double BudgetBalance
         {
-            get
+            get { return _budgetBalance; }
+            set
             {
-                var data = connection.Query<ExpensesModel>("SELECT * FROM ExpensesModel").ToList();
-                if (data.Count != 0)
+                if (_budgetBalance != value)
                 {
-                    var result = data.AsEnumerable().Sum(o => o.ExpenseCost);
-                    return result;
-                }
-                else
-                {
-                    return 0;
+                    _budgetBalance = value;
+                    OnPropertyChanged(); // Raise PropertyChanged event
                 }
             }
+        }
+
+        private double _expenses;
+        public double Expenses
+        {
+            get { return _expenses; }
+            set
+            {
+                if (_expenses != value)
+                {
+                    _expenses = value;
+                    OnPropertyChanged(); // Raise PropertyChanged event
+                }
+            }
+        }
+
+        private double _budgetPercent;
+        public double BudgetPercent
+        {
+            get { return _budgetPercent; }
+            set
+            {
+                if (_budgetPercent != value)
+                {
+                    _budgetPercent = value;
+                    OnPropertyChanged(); // Raise PropertyChanged event
+                }
+            }
+        }
+
+        private double _budget;
+        public double Budget
+        {
+            get { return _budget; }
+            set
+            {
+                if (_budget != value)
+                {
+                    _budget = value;
+                    OnPropertyChanged(); // Raise PropertyChanged event
+                }
+            }
+        }
+
+
+        private Color _buttonColor1;
+        public Color ButtonColor1
+        {
+            get { return _buttonColor1; }
+            set
+            {
+                if (_buttonColor1 != value)
+                {
+                    _buttonColor1 = value;
+                    OnPropertyChanged(); // Raise PropertyChanged event
+                }
+            }
+        }
+
+        private Color _buttonColor2;
+        public Color ButtonColor2
+        {
+            get { return _buttonColor2; }
+            set
+            {
+                if (_buttonColor2 != value)
+                {
+                    _buttonColor2 = value;
+                    OnPropertyChanged(); // Raise PropertyChanged event
+                }
+            }
+        }
+
+        private Color _buttonColor3;
+        public Color ButtonColor3
+        {
+            get { return _buttonColor3; }
+            set
+            {
+                if (_buttonColor3 != value)
+                {
+                    _buttonColor3 = value;
+                    OnPropertyChanged(); // Raise PropertyChanged event
+                }
+            }
+        }
+
+        private Color _buttonColor4;
+        public Color ButtonColor4
+        {
+            get { return _buttonColor4; }
+            set
+            {
+                if (_buttonColor4 != value)
+                {
+                    _buttonColor4 = value;
+                    OnPropertyChanged(); // Raise PropertyChanged event
+                }
+            }
+        }
+
+        public double getTotalExpense()
+        {
+            var data = connection.Query<ExpensesModel>("SELECT * FROM ExpensesModel").ToList();
+            if (data.Count != 0)
+            {
+                var result = data.AsEnumerable().Sum(o => o.ExpenseCost);
+                return result;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public void UpdateView()
+        {
+            Income = User.Income;
+            BudgetPercent = User.BudgetPercent;
+            Budget = (User.Income * User.BudgetPercent);
+            Expenses = getTotalExpense();
+            BudgetBalance = Budget - Expenses;
+            if(BudgetPercent == 0.5 )
+                ButtonColor1 = Colors.Green;
+            else ButtonColor1 = Colors.DarkGray;
+            if (BudgetPercent == 0.3)
+                ButtonColor2 = Colors.Green;
+            else ButtonColor2 = Colors.DarkGray;
+            if (BudgetPercent == 0.2)
+                ButtonColor3 = Colors.Green;
+            else ButtonColor3 = Colors.DarkGray;
+            if (BudgetPercent != 0.5 && BudgetPercent != 0.3 && BudgetPercent != 0.2)
+                ButtonColor4 = Colors.Green;
+            else ButtonColor4 = Colors.DarkGray;
         }
 
         public void SaveAndUpdateUser(UserModel model)
         {
             connection.Update(model);
+            UpdateView();
         }
+
         public void DeleteUser(UserModel model)
         {
             //If it has an Id, then we can delete it 
